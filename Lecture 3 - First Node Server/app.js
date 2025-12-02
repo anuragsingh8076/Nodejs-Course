7353ea0f2fdb8bc96ca823fb4dbd04e807d04458
anuragsingh8076 <anuragrajput9871595773@gmail.com>
2025-12-02 13:14:19 +0530

    Added Lecture 3 and Lecture 4

---
+++ Lecture 3 - First Node Server/app.js
@@ -0,0 +1,14 @@
+
+const http = require('http');
+ 
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+ // process.exit();  // Stops event loop 
+  
+});
+
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+  
+});
