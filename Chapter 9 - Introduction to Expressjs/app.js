36939a82ae5c193e9525090c5d775c1908890b6e
anuragsingh8076 <anuragrajput9871595773@gmail.com>
2026-01-28 16:20:26 +0530

    Refactor app.js to remove unused core module and clean up middleware comments

--- Chapter 9 - Introduction to Expressjs/app.js
+++ Chapter 9 - Introduction to Expressjs/app.js
@@ -1,6 +1,4 @@
 
-// Core Modules
-const http = require('http');
 
 // External Modules
 const express = require('express');
@@ -12,25 +10,35 @@ const requestHandler = require('./User')
 const app = express();
 
 // Adding Middleware
-app.use((req, res, next) => {
-  console.log("Came in first middleware", req.url, req.method);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log("Came in first middleware", req.url, req.method);
+//   next();
+// });
 
 
-app.use((req, res, next) => {
-  console.log("Came in Second middleware", req.url, req.method);
+// app.use((req, res, next) => {
+//   console.log("Came in Second middleware", req.url, req.method);
+//   res.send("<p>Welcome to complete coding Nodejs series</p>")
   
-});
+// });
+
+app.post('/sumbit-details', (req, res, next) => {
+  console.log("Second Middleware", req.url, req.method);
+  res.send("<p>Welcome to submit Details Page</p>");
+  
+})
+
+app.use('/', (req, res, next) => {
+  console.log("Second Middleware", req.url, req.method);
+  res.send("<p>Welcome to complete Coding</p>");
+  
+})
 
 
-
-
-const server = http.createServer(app);
 
 
 const PORT = 3003;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on address http://localhost:${PORT}`);
   
 });
