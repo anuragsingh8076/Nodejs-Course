575771e2588e1caf4c03a6a38535fc7b7e794337
anuragsingh8076 <anuragrajput9871595773@gmail.com>
2026-01-29 10:21:30 +0530

    Add package.json for Chapter 10 - Airbnb with dependencies and devDependencies

---
+++ Chapter 10 - Express DeepDive/app.js
@@ -0,0 +1,56 @@
+// External Modules
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("First Dummy Middleware",req.url, req.method );
+  next();
+})
+
+
+app.use((req, res, next) => {
+  console.log("Second Dummy Middleware", req.url, req.method);
+  next();
+})
+
+
+// app.use((req, res, next) => {
+//   console.log("Third Middleware", req.url, req.method);
+//   res.send("<h1>Welcome to complete coding</h1>");
+// })
+
+app.get("/", (req, res, next) => {
+  console.log("Handling / for GET", req.url, req.method);
+  res.send(`<h1>Welcome to complete coding</h1>`);
+  
+})
+
+app.get("/contact-us", (req, res, next) => {
+  console.log("Handling /contact-us for GET", req.url, req.method);
+  res.send(`<h1>Please give your details here</h1>
+    
+    <form action="/contact-us" method="POST">
+     <input type="text" name="name" placeholder="Enter your name" />
+      <input type="email" name="email" placeholder="Enter your email" />
+      <input type="submit" />
+    </form>
+    `);
+  
+});
+
+app.use(bodyParser.urlencoded());
+
+app.post("/contact-us", (req, res, next) => {
+  console.log("Handling /contact-us for POST", req.url, req.method, req.body);
+  
+  res.send(`<h1>Thank you for your submission</h1>`);
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+  
+});
